b102c85da3d901052957b8eeb3297324928e5dbc
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-24 21:37:10 +0200

    initial commit

--- src/main/scala/Sender.scala
+++ src/main/scala/Sender.scala
@@ -1,5 +1,6 @@
 class Sender(header: String) {
   def send(message: String): String = header + message
+  def numbers(n: Int): String = header + n
 }
 
 object Sender extends App {
--- src/test/scala/SenderSpec.scala
+++ src/test/scala/SenderSpec.scala
@@ -5,7 +5,15 @@ class SenderSpec extends AnyFlatSpec with should.Matchers {
   behavior of "Sender"
   it should "send message with header" in {
     val first = new Sender("Mr. ")
-    first.send("Robot") shouldBe "Mr. Robot"
+    val actual = first.send("Robot")
+    val expected = "Mr. Robot"
+    actual shouldBe expected
+
     first.send("Muscle") shouldBe "Mr. Muscle"
+  }
+  it should "send number with header" in {
+    val second = new Sender("Number ")
+    second.numbers(5) shouldBe "Number 5"
+    second.numbers(7) shouldBe "Number 7"
   }
 }
