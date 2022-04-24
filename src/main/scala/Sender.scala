6df5181364275455b405b6203620567f3dcf4b2c
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-24 21:31:29 +0200

    initial commit

--- src/main/scala/Sender.scala
+++ src/main/scala/Sender.scala
@@ -1,5 +1,5 @@
-class Sender (header: String) {
-  def send(message:String): String = header + message
+class Sender(header: String) {
+  def send(message: String): String = header + message
 }
 
 object Sender extends App {
--- src/test/scala/SenderSpec.scala
+++ src/test/scala/SenderSpec.scala
@@ -1,12 +1,10 @@
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
-import org.scalatest._
 
-class SenderSpec extends AnyFlatSpec with should.Matchers{
-import Sender._
+class SenderSpec extends AnyFlatSpec with should.Matchers {
   behavior of "Sender"
   it should "send message with header" in {
-    val first = new Sender ("Mr. ")
+    val first = new Sender("Mr. ")
     first.send("Robot") shouldBe "Mr. Robot"
     first.send("Muscle") shouldBe "Mr. Muscle"
   }
